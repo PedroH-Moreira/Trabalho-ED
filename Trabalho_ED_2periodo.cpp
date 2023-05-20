327031151b37e5ef5770cb940688b5df2c0df498
Pedro Henrique Cabral Moreira <99932007+PedroH-Moreira@users.noreply.github.com>
2023-05-20 18:20:43 -0300

    Update Trabalho_ED_2periodo.cpp
    Trabalho já com switch e case implementado, falta apenas implementar as funções

--- Trabalho_ED_2periodo.cpp
+++ Trabalho_ED_2periodo.cpp
@@ -3,7 +3,14 @@
 #include <string>
 #include <cstring>
 #include <stdexcept>
+#include <cstdlib>
 using namespace std;
+
+#ifdef _WIN32
+    const std::string CLEAR = "cls"; // Comando para limpar tela no Windows
+#else
+    const std::string CLEAR = "clear"; // Comando para limpar tela no Linux e macOS
+#endif
 
 class Dados
 {
@@ -19,6 +26,12 @@ class Dados
         Dados(string id, string Nome, string Cidade, string Esporte, string Event, string Noc);
         void conversao();
         void exibir();
+        //exibir entre o intervalo x e y
+        void exibir_intervalo(int x, int y);
+        void adicionar();
+        void alterar_elemento(string novo);
+        //trocar os elementos x e y de lugar
+        void trocar_elementos(int x, int y);
 };
 
 Dados::Dados()
@@ -52,7 +65,7 @@ void Dados::exibir()
         cout << " Esporte: " << d.mEsporte << " Evento: " << d.mEvento << " NOC: " << d.mNoc << endl;
     }
     fin.close();
-    remove ("base.bin");
+    //remove ("base.bin");
 }
 
 void Dados::conversao()
@@ -61,21 +74,16 @@ void Dados::conversao()
     ifstream ler("baseTest.csv");
     string LINHA;
     getline(ler, LINHA, '\n');
-
     //pegar a primeira linha da base <Id,Name,City,Sport,Event,NOC>
-    ofstream escrita("base.bin", ios::app | ios::binary);
-    char linha[29];
-    strcpy(linha, LINHA.c_str());
-    escrita.close();
 
     //enquanto o arquivo não chegar no fim, com o uso do getline e string string para a conversão
     //os dados são passados sempre no final do arquivo binário
+    int cont = 0, total = 271117;
     while(!ler.eof())
     {
         string idString, nome, cidade, esporte, evento, noc;
         getline(ler, idString, ',');
         getline(ler, nome, ',');
-
         if (nome.front() == '"' and nome.back() != '"') 
         {
             string nomeTemp;
@@ -85,7 +93,6 @@ void Dados::conversao()
                 nome+= ','+ nomeTemp;
             }
         }
-        
 
         //"Joseph ""Joe"" Angyal, Jr.",
         //"Tenley Emma Albright (-Gardiner, -Blakely)",
@@ -106,7 +113,6 @@ void Dados::conversao()
             }
         }
         ler >> noc;
-
         ler.ignore();
 
         //usando o construtor com parâmetro para associar aos dados da classe
@@ -115,19 +121,110 @@ void Dados::conversao()
         ofstream escrita("base.bin" , ios::app | ios::binary);
         escrita.write((char*)&d, sizeof(Dados));
         escrita.close();
+
+        //exibindo um contador do progresso na tela
+        int porcentagem = (cont + 1) * 100 / total;
+        cont++;
+        if(cont % 5000 == 0)
+        {
+            cout << "Convertendo o arquivo para binario..\nProgresso = " << porcentagem << "%";
+        } else if(cont % 4999 == 0)
+                {
+                    system(CLEAR.c_str());
+                }
     }
 
     ler.close();
+    system(CLEAR.c_str());
+    cout << "Arquivo CSV convertido para binario com sucesso\n";
+}
+
+void Dados::exibir_intervalo(int x, int y){}
+
+void Dados::alterar_elemento(string novo){}
+
+void Dados::trocar_elementos(int x, int y)
+{
 
 }
 
+void Dados::adicionar(){}
+
+void apresentacao()
+{
+    cout << " - - - - - - - - - TRABALHO DE ED - - - - - - - - - \n";
+    cout << "Segundo periodo --- Universidade Federal de Lavras \n";
+    cout << "Participantes: Esther Silva de Magalhaes, \n";
+    cout << "Italo Alves Rabelo, \n";
+    cout << "Pedro Henrique Cabral Moreira. \n";
+    cout << "Professor: Renato Ramos da Silva \n";
+    cout << "Base de dados: \"data_athlete_event\"\n";
+    cout << "Digite enter para comecar \n";
+    char c; cin.get(c);
+    system(CLEAR.c_str());
+}
 
 
 int main()
 {
-    Dados dados;
-    dados.conversao();
-    dados.exibir();
+    if("base.bin")
+        remove("base.bin");
 
+    apresentacao();
+    Dados d;
+    d.conversao();
+    char key = 'l';
+
+    while(key != 's')
+    {
+        cout << "Escolha a funcao desejada \n";
+        cout << "e - Exibir todo o arquivo.\n";
+        cout << "i - Exibir um intervalo especifico do arquivo.\n";
+        cout << "a - Alterar elemento.\n";
+        cout << "t - Trocar elementos.\n";
+        cout << "d - Adicionar elemento.\n";
+        cout << "s - Fechar o programa.\n";
+        cin >> key;
+        string novoNome;
+        int x, y;
+        switch(key)
+        {
+            case 'e':
+                d.exibir();
+                break;
+            case 'i':
+                cout << "Digite o intervalo desejado - Exemplo: de x ate y\n";
+                cout << "x = "; cin >> x;
+                cout << "y = "; cin >> y;
+                cout << endl;
+                cin.ignore();
+                d.exibir_intervalo(x,y);
+                break;
+            case 'a':
+                cin.ignore();
+                cout << "Digite a posicao que deseja alterar o nome.\n";
+                cin >> x;
+
+                cin.ignore();
+                cout << "Digite o novo nome que deseja atribuir a posicao " << x << endl;
+                getline(cin,novoNome);
+                d.alterar_elemento(novoNome);
+                break;
+            case 't':
+                cin.ignore();
+                cin >> x;
+                cin >> y;
+                d.trocar_elementos(x,y);
+                break;
+            case 'd':
+                cin.ignore();
+                d.adicionar();
+            default:
+
+                break;
+        }
+        system(CLEAR.c_str());
+    }
+    cout << "\nPrograma encerrado.";
     return 0;
 }
